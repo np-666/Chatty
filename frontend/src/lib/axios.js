f43adee8e4df413247d01282936b32af502156ff
Nick Perry <nicks.sh@outlook.com>
2025-05-28 20:18:03 +0530

    Update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const backend = import.meta.env.VITE_BACKEND_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${backend}/api`,
     withCredentials: true,
 });
